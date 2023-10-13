fe364c4eb8b83fbce3e01381e836e8313ee6b5ff
yaoyao <i@yaoyao.io>
2023-10-13 22:16:25 +0800

    first commit

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,12 +1,13 @@
 import {Theme} from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import './styles/index.scss'
-
+// @ts-ignore
+import Layout from "./components/Layout.vue";
 import {customConfigProvider} from "./configProvider";
 
 export default {
   ...DefaultTheme,
-  // Layout: customConfigProvider(Layout),
+  Layout: customConfigProvider(Layout),
   enhanceApp({app}) {
   },
 } as Theme;
