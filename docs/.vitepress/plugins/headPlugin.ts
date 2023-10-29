1a16d9ccec46cebf364f7003110ae94c6505f8e4
yaoyao <i@yaoyao.io>
2023-10-29 21:34:42 +0800

    update img

--- docs/.vitepress/plugins/headPlugin.ts
+++ docs/.vitepress/plugins/headPlugin.ts
@@ -7,12 +7,12 @@ export function handleHeadMeta(context: TransformContext): HeadConfig[] {
   const ogUrl: HeadConfig = ["meta", {property: "og:url", content: addBase(pageData.relativePath.slice(0, -3)) + '.html'}]
   const ogTitle: HeadConfig = ["meta", {property: "og:title", content: pageData.title || context.title}];
   const ogDescription: HeadConfig = ["meta", {property: "og:description", content: pageData.description || context.description}];
-  const ogImage: HeadConfig = ["meta", {property: "og:image", content: pageData.frontmatter.image || "https://blog.yaoyao.io/logo2.jpeg"}];
+  const ogImage: HeadConfig = ["meta", {property: "og:image", content: pageData.frontmatter.image || "https://blog.yaoyao.io/img/logo/logo.jpeg"}];
   head.push(ogUrl, ogTitle, ogDescription, ogImage);
 
   const twitterCard: HeadConfig = ["meta", {name: "twitter:card", content: "summary_large_image"}];
   const twitterTitle: HeadConfig = ["meta", {name: "twitter:title", content: pageData.title || context.title}];
-  const twitterImage: HeadConfig = ["meta", {name: "twitter:image", content: pageData.frontmatter.image || "https://blog.yaoyao.io/logo2.jpeg"}];
+  const twitterImage: HeadConfig = ["meta", {name: "twitter:image", content: pageData.frontmatter.image || "https://blog.yaoyao.io/img/logo/logo.jpeg"}];
   const twitterDescription: HeadConfig = ["meta", {name: "twitter:description", content: pageData.description || context.description}];
   head.push(twitterCard, twitterTitle, twitterImage, twitterDescription);
 
